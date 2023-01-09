2d54387d90fba78d5f3a114247790588f0a3ea24
ANTHONY WOO <anthony.woo14@gmail.com>
2023-01-09 15:42:09 -0700

    called and tested function with console log

--- app.js
+++ app.js
@@ -11,36 +11,30 @@ const totalEl = document.getElementById('total');
 const lossesEl = document.getElementById('losses');
 const winsEl = document.getElementById('wins');
 
-
 let correctGuesses = 0;
 let totalGuesses = 0;
+let incorrectGuess = 0;
 
 shedButton.addEventListener('click', () => {
     // get a random item to call the 'correct spot'
-
+    const correctSpot = getRandomHidingSpot();
     // call the handleGuess function with the correct parameters (the user's guess and the "correct" hiding place) to do DOM work
+    handleGuess('shed', correctSpot);
 });
 
 treeButton.addEventListener('click', () => {
     // get a random item to call the 'correct spot'
-
     // call the handleGuess function with the correct parameters (the user's guess and the "correct" hiding place) to do DOM work
 });
 
 boulderButton.addEventListener('click', () => {
     // get a random item to call the 'correct spot'
-
     // call the handleGuess function with the correct parameters (the user's guess and the "correct" hiding place) to do DOM work
 });
 
-
 function getRandomHidingSpot() {
     // initialize state
-    const hidingPlaces = [
-        'tree',
-        'shed',
-        'boulder'
-    ];
+    const hidingPlaces = ['tree', 'shed', 'boulder'];
 
     const index = Math.floor(Math.random() * hidingPlaces.length);
 
@@ -50,17 +44,12 @@ function getRandomHidingSpot() {
 }
 
 function handleGuess(userGuess, correctSpot) {
+    console.log('hello');
     // first, right after clicking, we need to remove the emoiji face from the previous hiding place that way we don't end up with more than one emoji face
-
     // we can do that by removing the .face class from all containers
-
     // then increment the guesses
-
     // then use getElementById and the correctSpot string to grab the appropriate container from the DOM
-
     // then add the .face css class to that element so that the face shows up
-
     // then if the user guess is correct, increment the correct guesses
-
     // update the DOM to show the new value of wins, losses and total guesses to the user
 }
